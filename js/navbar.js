d7fa2225137f116966d28ab68f55ebc19a18e947
RealUnstoppable <130341423+RealUnstoppable@users.noreply.github.com>
2026-02-03 16:00:34 -0500

    Navba

--- js/navbar.js
+++ js/navbar.js
@@ -7,7 +7,7 @@ export function loadNavbar() {
     <nav class="navbar">
         <a href="index.html" class="nav-logo">un<span></span></a>
         <ul class="nav-links">
-            <li><a href="index.html#bento">Overview</a></li>
+            
             <li><a href="index.html#unstoppable">Unstoppable</a></li>
             <li><a href="index.html#dts">Dreams TimeSkip</a></li>
             <li><a href="harmonytunes.html">HarmonyTunes</a></li>
